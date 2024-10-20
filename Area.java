74ab96b88cbaa99fcaa4d2305f62f472494a05ab
Vikasdraj <vikasdraj244@gmail.com>
2024-10-21 00:44:09 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,18 @@
+public class Area 
+{
+	public static double circle ()
+	{
+		int r=5;
+		final double pi=3.14;
+		double a=pi*r*r;
+		System.out.println(a);
+		return a;
+		//System.out.println (a) This is not possible as the value of variable a is returned
+	}
+	    public static void main(String[] args) 
+	{
+			 double x=circle();
+		     System.out.println(x);
+			 //System.out.println (a) This is not right because variable "a" is 
+	}
+}
